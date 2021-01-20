1905812f447d17a6f27d884f273126586d66c1a6
Valentyna-Chudik <valentyna_chudik@ukr.net>
2021-01-20 18:25:07 +0200

    Local Backend added (json-server), Persist code deleted

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,43 +1,18 @@
 // REACT TOOLKIT
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+
 import contactsReducer from './contacts/contacts-reducer';
 
-const middleware = [
-  ...getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
-  logger,
-];
-
-const contactsPersistConfig = {
-  key: 'contacts',
-  storage,
-  blacklist: ['filter'],
-};
+const middleware = [...getDefaultMiddleware(), logger];
 
 export const store = configureStore({
   reducer: {
-    contacts: persistReducer(contactsPersistConfig, contactsReducer),
+    contacts: contactsReducer,
   },
   middleware,
   devTools: process.env.NODE_ENV === 'development',
 });
-
-export const persistor = persistStore(store);
 
 // REDUX
 // import { createStore, combineReducers } from 'redux';
